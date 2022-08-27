fe2c3d5f1e298364d03ba5769b214626d66c55d2
LUXI SHANU C G <85391671+luxi2021@users.noreply.github.com>
2022-08-27 22:51:06 +0530

    Add files via upload

---
+++ gritx/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './App.css';
+
+ function App(){
+  
+    return (
+     <div>
+  
+  <div class="bg-img">   
+    <div>
+     <div class="g1">
+        <div id="vertical-text" className="cont1">
+          <h1><span>A<br></br>B<br></br>O<br></br>U<br></br>T <br></br></span>E<br></br>V<br></br>E<br></br>N<br></br>T<br></br></h1>
+        </div>
+     </div>
+     
+    </div>
+    <div class="vl"></div>
+   </div>
+
+ <div class="cont">
+ <h3>The National Service Scheme (NSS) is an Indian government sector public service program conducted by the Ministry of Youth Affairs and Sports of the Government of India. Popularly known as NSS, the scheme was launched in Gandhiji's Centenary year in 1969. Aimed at developing student's personality through community service, NSS is a voluntary association of young people in Colleges, Universities and at +2 level working for a campus-community (esp. Villages) linkage.&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;</h3>
+ &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+ <div class="botalg">
+ <div class="date">03&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;</div>
+ <div class="rounds">ROUNDS</div>
+ <div class="notwo">02&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;</div>
+ <div class="winners">WINNERS</div>
+ <div class="nofifty">50+&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;</div>
+ <div class="register">REGISTRATIONS</div></div>
+</div>
+</div>
+
+
+       
+    );
+
+}
+
+export default App;
