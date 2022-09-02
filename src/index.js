06de24affe3dd5f96a8e5674f4bfa3cc6c2746f6
Gokulaa-krishnaa <66513639+Gokulaa-krishnaa@users.noreply.github.com>
2022-09-02 19:59:41 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from './components/home';
 import EventsSlide from './components/eventsslide';
+import Campusambassador from './components/campusambassador';
+import Busroute from './components/busroute';
+import Description from './components/Description';
+import Details from './components/Details';
+import Descriptionfooter from './components/Descriptionfooter';
+import Mindtwisterdetails from './contents/mindtwisterdetails';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar />
-     <Home />
-     <EventsSlide />
+    <BrowserRouter>
+    <Routes >
+      <Route path="/" element={
+        <>
+        <Navbar />
+        <Home />
+        <EventsSlide />
+        <Campusambassador />
+        <Busroute />
+        <Footer />
+        </>
+      } />
+
+    </Routes>
+    <Routes >
+      <Route path="/events" element={
+        <>
+          <Description Descriptioncontent={Mindtwisterdetails}/>
+          <Details />
+          <Descriptionfooter />
+        </>
+      } />
+
+    </Routes>
+    </BrowserRouter>
+   
   </React.StrictMode>
 );
